6f1bce56234c26c7bc26a49982b3142bb49a5312
Sergiy <mirochnyk.sergiy@gmail.com>
2025-07-30 18:08:14 +0200

    feat: implement customizable Button component with multiple variants and sizes

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -58,7 +58,7 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, type = "button", ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     
     // Use CSS classes instead of inline styles for themed buttons
@@ -67,6 +67,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        type={type}
         {...props}
       />
     )
